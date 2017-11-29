02258cc329ade78c5a1eff25d81b667dd1f59812
TommyLemon <1184482681@qq.com>
2017-11-29 22:37:59 +0800

    优化获取文档的角色错误提示

--- js/main.js
+++ js/main.js
@@ -506,6 +506,7 @@
             {
               'Document[]': {
                 'Document': {
+                  '@role': 'login',
                   '@order': 'version-,date-'
                 }
               }
