51082397c221f60bf575f58131168c8899003a98
TommyLemon <1184482681@qq.com>
2018-02-24 19:43:40 +0800

    请求JSON注释：新增逻辑运算的注释

--- apijson/CodeUtil.js
+++ apijson/CodeUtil.js
@@ -866,69 +866,79 @@ var CodeUtil = {
         return table.TABLE_COMMENT;
       }
 
+
+      //功能符 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
       if (columnName.endsWith("()")) {//方法，查询完后处理，先用一个Map<key,function>保存？
         return '远程函数';
       }
 
-      var prefix;
-      var at = '';
-      var key;
 
+      var at = '';
       if (columnName.endsWith("@")) {//引用，引用对象查询完后处理。fillTarget中暂时不用处理，因为非GET请求都是由给定的id确定，不需要引用
         at = '引用赋值';
         columnName = columnName.substring(0, columnName.length - 1);
       }
 
+      var fun;
+      var key;
       if (columnName.endsWith("$")) {//搜索，查询时处理
-        prefix = '模糊搜索';
+        fun = '模糊搜索';
         key = columnName.substring(0, columnName.length - 1);
       }
       else if (columnName.endsWith("?")) {//匹配正则表达式，查询时处理
-        prefix = '正则匹配';
+        fun = '正则匹配';
         key = columnName.substring(0, columnName.length - 1);
       }
       else if (columnName.endsWith("{}")) {//被包含，或者说key对应值处于value的范围内。查询时处理
-        prefix = '匹配 选项/条件';
+        fun = '匹配 选项/条件';
         key = columnName.substring(0, columnName.length - 2);
       }
       else if (columnName.endsWith("<>")) {//包含，或者说value处于key对应值的范围内。查询时处理
-        prefix = '包含选项';
+        fun = '包含选项';
         key = columnName.substring(0, columnName.length - 2);
       }
       else if (columnName.endsWith("+")) {//延长，PUT查询时处理
         //if (method == PUT) {//不为PUT就抛异常
-        prefix = '增加 或 扩展	';
+        fun = '增加 或 扩展	';
         key = columnName.substring(0, columnName.length - 1);
         //}
       }
       else if (columnName.endsWith("-")) {//缩减，PUT查询时处理
         //if (method == PUT) {//不为PUT就抛异常
-        prefix = '减少 或 去除';
+        fun = '减少 或 去除';
         key = columnName.substring(0, columnName.length - 1);
         //}
       }
       else {
-        prefix = '';
+        fun = '';
         key = new String(columnName);
       }
 
 
-      var last = null;//不用Logic优化代码，否则 key 可能变为 key| 导致 key=value 变成 key|=value 而出错
+      var logic = '';
       //if (RequestMethod.isQueryMethod(method)) {//逻辑运算符仅供GET,HEAD方法使用
-      last = key.length <= 0 ? "" : key.substring(key.length - 1);
-      if ("&" == last || "|" == last || "!" == last) {
-        key = key.substring(0, key.length - 1);
-      } else {
-        last = null;//避免key + StringUtil.getString(last)错误延长
+      if (key.endsWith("&")) {
+        logic = '符合全部';
       }
+      else if (key.endsWith("|")) {
+        logic = '符合任意';
+      }
+      else if (key.endsWith("!")) {
+        logic = '都不符合';
+      }
+      else {}
       //}
 
-      if (StringUtil.isName(key) == false) {
-        return ' ! 字符' + key + '不合法！';
+      if (logic.length > 0) {
+        key = key.substring(0, key.length - 1);
       }
 
+      if (StringUtil.isName(key) == false) {
+        return ' ! 字符 ' + key + ' 不合法！';
+      }
 
-
+      //功能符 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
       columnList = item['Column[]'];
@@ -945,7 +955,9 @@ var CodeUtil = {
           continue;
         }
 
-        var p = (at.length <= 0 ? '' : at + ' > ') + (prefix.length <= 0 ? '' : prefix + ' > ');
+        var p = (at.length <= 0 ? '' : at + ' > ')
+          + (fun.length <= 0 ? '' : fun + ' > ')
+          + (logic.length <= 0 ? '' : logic + ' > ');
         return (p.length <= 0 ? '' : p + key + ': ') + column.COLUMN_COMMENT;
       }
 
