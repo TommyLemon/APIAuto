a93909430531e2470105cb904a256f11fc033aa9
TommyLemon <1184482681@qq.com>
2021-03-21 00:31:21 +0800

    零代码回归测试：无对比标准时状态码在 [400, 599] 内也断言为失败

--- apijson/JSONResponse.js
+++ apijson/JSONResponse.js
@@ -354,10 +354,10 @@ var JSONResponse = {
     // }
 
     if (tCode == null) {
-      if (typeof rCode == 'number' && rCode%10 != 0) {
+      if (typeof rCode == 'number' && (rCode%10 != 0 || (rCode >= 400 && rCode < 600))) {
         return {
           code: JSONResponse.COMPARE_CODE_CHANGE, //未上传对比标准
-          msg: '没有校验标准，且状态码不是 0, 200 等以 0 结尾的数',
+          msg: '没有校验标准，且状态码在 [400, 599] 内或不是 0, 200 等以 0 结尾的数',
           path: folder == null ? '' : folder
         };
       }
