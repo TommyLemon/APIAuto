c9b8eed78063088fa1668e15448cc3c82288b6c3
TommyLemon <1184482681@qq.com>
2022-07-06 01:29:36 +0800

    Admin：解决请求失败后仍在 loading

--- js/main.js
+++ js/main.js
@@ -3905,6 +3905,8 @@
             App.onResponse(url, res, null)
           })
           .catch(function (err) {
+            App.isLoading = false
+
             log('send >> error:\n' + err)
             if (isAdminOperation) {
               App.delegateId = null
